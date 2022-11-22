743707aeac2ee9dc4d401add8c01df86cbf0cc1c
luoxiaoyu <qvyue@qq.com>
2022-11-22 10:29:55 +0800

    regex

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
+  url: 'https://qvyue.github.io/luoxiaoyu.github.io/',
+  baseUrl: 'https://qvyue.github.io/luoxiaoyu.github.io/',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'qvyue', // Usually your GitHub org/user name.
+  projectName: 'luoxiaoyu.github.io', // Usually your repo name.
+  deploymentBranch: 'github-pages',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
